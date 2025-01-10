adedfcedd5a23cf7f6cf8dc2ece10adddf380b69
AlexVilkins <hetagpuhaev@mail.ru>
2025-01-10 12:01:19 +0300

    add vite settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  },
   resolve: {
     alias: {
       "@pages": "/src/pages",
